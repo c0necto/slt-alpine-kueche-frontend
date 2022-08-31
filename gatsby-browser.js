9bceb03ac3bb0b9d1a9a68021ff2d7f1d6fffd46
Patrik Wartbichler <p.wartbichler@conecto.at>
2022-08-31 09:52:58 +0200

    cookiebot browser var test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,42 +11,52 @@ import '@fontsource/prata';
 
 import React from 'react';
 import { CookiesProvider } from "react-cookie";
+// https://www.npmjs.com/package/react-cookie
+import {useCookies} from "react-cookie"
 
 import SimpleReactLightbox from 'simple-react-lightbox';
 // USE THE IMPORT BELOW INSTEAD IF YOU ARE USING THE PRO VERSION
 // import SimpleReactLightbox from 'simple-react-lightbox-pro'
 
 // eslint-disable-next-line react/prop-types
-export const wrapRootElement = ({ element }) => (
+/*export const wrapRootElement = ({ element }) => (
     <CookiesProvider>
         <SimpleReactLightbox>{element}</SimpleReactLightbox>
     </CookiesProvider>
-)
+)*/
 
-// https://www.npmjs.com/package/react-cookie
-import {useCookies} from "react-cookie"
-const [cookies, setCookie] = useCookies(['agreedtoyoutube']);
+export const wrapRootElement = ({element}) => {
 
-const handleClick = () => {
-    setCookie("agreedtoyoutube", true, {path: "/"})
-    console.log("cookie set")
-}
+    // const [cookies, setCookie] = useCookies(['agreedtoyoutube']);
 
-const isBrowser = () => typeof window !== "undefined";
-if ( isBrowser() ) {
-    // if Cookiebot is defined
-    if (typeof window.Cookiebot !== "undefined") {
-        console.log(window.Cookiebot.consent.marketing)
+    const handleClick = () => {
+        // setCookie("agreedtoyoutube", true, {path: "/"})
+        console.log("cookie set")
+    }
 
-        window.addEventListener('CookiebotOnAccept', function (e) {
-            if (window.Cookiebot.consent.marketing)
-            {
+    const isBrowser = () => typeof window !== "undefined";
+    if ( isBrowser() ) {
+        // if Cookiebot is defined
+        if (typeof window.Cookiebot !== "undefined") {
+            console.log(window.Cookiebot.consent.marketing)
+
+            window.addEventListener('CookiebotOnAccept', function (e) {
+                if (window.Cookiebot.consent.marketing)
+                {
+                    handleClick()
+                }
+            }, false);
+
+            if ( window.Cookiebot.consent.marketing) {
                 handleClick()
             }
-        }, false);
-
-        if ( window.Cookiebot.consent.marketing) {
-            handleClick()
         }
     }
+
+    return (
+        <CookiesProvider>
+            <SimpleReactLightbox>{element}</SimpleReactLightbox>
+        </CookiesProvider>
+    )
 }
+
