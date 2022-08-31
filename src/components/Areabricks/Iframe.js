55dad883f29459fb8e100a2c9a58ba068222627f
Patrik Wartbichler <p.wartbichler@conecto.at>
2022-08-31 13:46:25 +0200

    cookiebot context provider

--- src/components/Areabricks/Iframe.js
+++ src/components/Areabricks/Iframe.js
@@ -51,7 +51,7 @@ const IframeAreabrick = props => {
     if ( isBrowser() ) {
         if (typeof window.Cookiebot !== "undefined") {
 
-            ['CookiebotOnAccept'].forEach(event => {
+            ['CookiebotOnAccept', 'CookiebotOnDecline'].forEach(event => {
                 window.addEventListener(event, ev => {
                     if (window.Cookiebot.consent.marketing)
                     {
