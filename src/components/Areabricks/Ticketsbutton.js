a888eab43c7efcffffe0adf3df44a23cf2bde6d3
Patrik Wartbichler <p.wartbichler@conecto.at>
2023-04-25 16:37:19 +0200

    Completed Ticketsbutton output

--- src/components/Areabricks/Ticketsbutton.js
+++ src/components/Areabricks/Ticketsbutton.js
@@ -1,19 +1,19 @@
-import React from "react"
+import React, { memo } from "react";
 import ContentArea from '~components/ContentArea/ContentArea'
 import Container from  '~components/Container/Container'
 import Ticketsbutton from '~components/Ticketsbutton/Ticketsbutton'
 
-
-const TicketsbuttonAreabrick = props => {
-    const { data } = props
-    console.log('data: ', data)
-    return (
-        <ContentArea className={'bottom80'}>
-            <Container variant={'narrow'}>
-                <Ticketsbutton></Ticketsbutton>
-            </Container>
-        </ContentArea>
-    )
-}
+const TicketsbuttonAreabrick = memo(({ elements: { title, subtitle, link } }) => (
+    <ContentArea className="bottom80">
+        <Container variant={'narrow'}>
+            <Ticketsbutton
+                title={title?.text}
+                subtitle={subtitle?.text}
+                to={link?.linkData?.path}
+                target={link?.linkData?.windowTarget}
+            />
+        </Container>
+    </ContentArea>
+));
 
 export default TicketsbuttonAreabrick
--- src/components/Ticketsbutton/Ticketsbutton.js
+++ src/components/Ticketsbutton/Ticketsbutton.js
@@ -1,29 +1,34 @@
-import React from "react"
-import TicketIcon from "./tickets.svg"
+import React, { memo } from "react"
 import * as styles from "./Ticketsbutton.module.scss"
+import TicketIcon from "./tickets.svg"
 
-const ExternalLink = ({to, title, subtitle, target, children}) => (
-    <a href={to}
-       target={target}
-       className={styles.ticketsbutton}
-       rel="noopener noreferrer">
+const ExternalLink = memo(({ to, title, target, children }) => (
+    <a
+        href={to}
+        target={target}
+        title={title}
+        aria-label={title}
+        className={styles.ticketsbutton}
+        rel="noopener noreferrer"
+    >
         {children}
     </a>
-);
+))
 
-const Ticketsbutton = props => {
-    console.log(props)
+const Ticketsbutton = memo(({ title, subtitle, to, target }) => {
+    const buttonText = (
+        <div className={styles.ticketsbuttonText}>
+            <strong>{title}</strong>
+            <div>{subtitle}</div>
+        </div>
+    )
+
     return (
-        <ExternalLink to={'#'}>
-            <i>
-                <img src={TicketIcon} alt={'#'}/>
-            </i>
-            <div className={styles.ticketsbuttonText}>
-                <strong>Jetzt Tickets kaufen!</strong>
-                <div>Der Early Bird Vorverkaufspreis gilt bis 30.06.2023</div>
-            </div>
+        <ExternalLink to={to} title={title} target={target}>
+            <i><img src={TicketIcon} alt={title} /></i>
+            {buttonText}
         </ExternalLink>
     )
-}
+})
 
 export default Ticketsbutton
